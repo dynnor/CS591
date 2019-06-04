ad4e3cc07f8966b6379968cf610f8de513f977bb
dynnor <dynnor@bu.edu>
2019-06-04 17:20:47 -0400

    submit to gradescope

--- PS3/ps3/routes/ps3.js
+++ PS3/ps3/routes/ps3.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET ps3 page. */
 router.get('/', function(req, res, next) {
-    res.render('ps3', { string: 'Hey now' });
+    res.render('ps3', { title: 'Hey now' });
 });
 
 module.exports = router;
+
+
+
+router.post('/', function(req, res, next){
+   // console.log(req)
+    res.render('ps3', { title: req.body.title, length: req.body.title.length});
+    }
+
+);
+
+
